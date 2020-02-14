29b232e55cb21f943819d63349c67f933dd12c58
terajh <terajoohyun@ajou.ac.kr>
2020-02-14 15:44:06 +0900

    200214 add comment function

--- routes/users.js
+++ routes/users.js
@@ -14,10 +14,51 @@ router.get('/', function(req, res, next) {
     var AuthStatusui = auth.statusUI(req, res);
     db.query(`SELECT * FROM users`, (error, results) => {
         if (error) throw error;
-        res.render('user_board', {
+        res.render('users', {
             title: results
         });
     });
 });
+router.get('/board', (req, res, next) => {
+    var AuthStatusui = auth.statusUI(req, res);
+    db.query('SELECT * FROM userboard', (error, results) => {
+        if (error) throw error;
+        if (auth.IsOwner(req, res)) {
+            res.render('users_board', {
+                AuthStatusui: AuthStatusui,
+                usr: req.user.nickname,
+                title: results
+            });
+        } else {
+            res.render('user_board', {
+                AuthStatusui: AuthStatusui,
+                title: results,
+                usr: ""
+            });
+        }
+
+    })
+})
+router.post('/board_process', (req, res, next) => {
+    var post = req.body;
+    var description = post.description;
+    var AuthStatusui = auth.statusUI(req, res);
+    console.log('create comment');
+    db.query(`INSERT INTO userboard (username,description,created) VALUES('${req.user.nickname}', '${description}',NOW())`, (error, userboard) => {
+        res.redirect('/users/board')
+    });
+})
+
+router.post('/delete_comment', (req, res, next) => {
+    var post = req.body;
+    var delId = post.id;
+    var AuthStatusui = auth.statusUI(req, res);
+    // console.log('process');
+    db.query(`DELETE FROM userboard WHERE id=${delId}`, (error, result) => {
+        if (error) throw error;
+        res.redirect('/');
+    });
+})
+
 
 module.exports = router;
