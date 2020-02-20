701cfd3a25496b51fb34ba0cbff10dfa927436bf
Ubuntu <ubuntu@ip-172-31-43-148.ap-northeast-2.compute.internal>
2020-02-20 04:20:46 +0000

    200220 user_board modified

--- routes/users.js
+++ routes/users.js
@@ -21,8 +21,9 @@ router.get('/', function(req, res, next) {
 });
 router.get('/board', (req, res, next) => {
     var AuthStatusui = auth.statusUI(req, res);
-    db.query('SELECT * FROM userboard', (error, results) => {
+    db.query(`SELECT username,comment,created FROM userboard`, (error, results) => {
         if (error) throw error;
+	console.log(results);
         if (auth.IsOwner(req, res)) {
             res.render('users_board', {
                 AuthStatusui: AuthStatusui,
@@ -30,7 +31,7 @@ router.get('/board', (req, res, next) => {
                 title: results
             });
         } else {
-            res.render('user_board', {
+            res.render('users_board', {
                 AuthStatusui: AuthStatusui,
                 title: results,
                 usr: ""
@@ -44,7 +45,7 @@ router.post('/board_process', (req, res, next) => {
     var description = post.description;
     var AuthStatusui = auth.statusUI(req, res);
     console.log('create comment');
-    db.query(`INSERT INTO userboard (username,description,created) VALUES('${req.user.nickname}', '${description}',NOW())`, (error, userboard) => {
+    db.query(`INSERT INTO userboard (username,comment,created) VALUES('${req.user.nickname}', '${description}',NOW())`, (error, userboard) => {
         res.redirect('/users/board')
     });
 })
